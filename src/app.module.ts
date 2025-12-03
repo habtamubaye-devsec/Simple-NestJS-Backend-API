fd1670d1e13eced234286a8ebbfe478eaa6da484
habtamubaye-devsec <habtamubaye2127gmail.com>
2025-12-03 22:32:42 +0300

    Remove Licence from the README.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ReportController } from './report/report.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, ReportController],
   providers: [AppService],
 })
 export class AppModule {}
