88dda87a0b0a262813844d3b9fda31489ab06bae
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-03 23:04:53 +0300

    Add ExpwnseController to AppModule controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportController } from './report/report.controller';
+import { ExpwnseController } from './expwnse/expwnse.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, ReportController],
+  controllers: [AppController, ReportController, ExpwnseController],
   providers: [AppService],
 })
 export class AppModule {}
