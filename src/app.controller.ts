0b170a4a5905a9c7db524e63a72245f4f544ad8b
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-09 22:48:38 +0300

    Refactor AppController to manage reports with CRUD operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("report/:type")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/income")
+  getAllIncomeReport(){
+    return []
+  }
+
+  @Get(":id")
+  getAllIncomeReportById() {
+    return {};
+  }
+
+  @Post()
+  createReports() {
+    return "Created"
+  }
+
+  @Put(':id')
+  updateReports() {
+    return "Updated"
+  }
+
+  @Delete(":id")
+  deleteReports() {
+    return "Deleted"
   }
 }
