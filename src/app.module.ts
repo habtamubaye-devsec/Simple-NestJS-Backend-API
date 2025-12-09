1f149fe2642b78efff4823c3193e7c755c1f0724
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-09 22:50:17 +0300

    Remove unused imports and controllers from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportController } from './report/report.controller';
-import { ExpwnseController } from './expwnse/expwnse.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, ReportController, ExpwnseController],
+  controllers: [AppController ],
   providers: [AppService],
 })
 export class AppModule {}
