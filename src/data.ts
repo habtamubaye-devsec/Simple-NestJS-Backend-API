ba72a170e20ea5e7479ed91b0653e4e0e687d78e
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-11 14:57:48 +0300

    Refactor data structure to define ReportType enum and enhance report interface

--- src/data.ts
+++ src/data.ts
@@ -1,19 +1,44 @@
-const data = {
-    report: []
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
 }
 
 interface Data {
-    report:{
-        id: string;
-        source: string;
-        amount: number;
-        created_at: Data;
-        updated_at: Date;
-        type: ReportType ;
-    }[]
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Data;
+    updated_at: Date;
+    type: ReportType;
+  }[];
 }
 
-enum ReportType{
-    INCOME = "income",
-    EXPENSE = "expense"
-}
+export const data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7809,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'Youtube',
+      amount: 2999,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 6588,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
