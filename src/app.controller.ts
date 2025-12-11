b0dcbec1d1d589678dec1943c80ab73d5040c171
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-11 14:51:30 +0300

    Refactor AppController to enhance report handling and add missing CRUD operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,83 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { data, ReportType } from './data';
+import { v4 as uuid } from 'uuid';
 
-@Controller("report/:type")
+@Controller('report/:type')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/income")
-  getAllIncomeReport(){
-    return []
+  @Get()
+  getAllIncomeReport(@Param('type') type: string) {
+    console.log(type);
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
-  @Get(":id")
-  getAllIncomeReportById() {
-    return {};
+  @Get(':id')
+  getAllIncomeReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    console.log(id, type);
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
-  createReports() {
-    return "Created"
+  createReports(@Body() body: { amount: number; source: string }, @Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const newReport = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: reportType,
+    };
+    data.report.push(newReport);
+
+    return 'Created';
   }
 
   @Put(':id')
-  updateReports() {
-    return "Updated"
+  updateReports(@Body() body: { amount: number; source: string }, @Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const updatedReportIndex = data.report.findIndex((report => report.id === id && report.type === reportType));
+    if (updatedReportIndex === -1) {
+      return 'Report not found';
+    }
+    const existingReport = data.report[updatedReportIndex];
+    const updatedReport = {
+      ...existingReport,
+      source: body.source,
+      amount: body.amount,
+      updated_at: new Date(),
+    };
+    data.report[updatedReportIndex] = updatedReport;
+    return 'Updated';
   }
 
-  @Delete(":id")
-  deleteReports() {
-    return "Deleted"
+  @Delete(':id')
+  deleteReports(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportIndex = data.report.findIndex((report) => report.id === id && report.type === reportType);
+    if (reportIndex === -1) {
+      return 'Report not found';
+    }
+    data.report.splice(reportIndex, 1);
+    return 'Deleted';
   }
 }
