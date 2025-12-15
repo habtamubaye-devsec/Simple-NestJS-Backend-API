59d48de6d08485b6d005418ac9a8350223314042
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-15 10:48:52 +0300

    Add DTOs for report creation, updating, and response handling

---
+++ src/dtos/report.dtos.ts
@@ -0,0 +1,54 @@
+import { Exclude, Expose } from 'class-transformer';
+import {
+  IsNumber,
+  IsString,
+  IsPositive,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+import { ReportType } from 'src/data';
+
+
+export class CreateReportDto {
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  source: string;
+}
+
+export class UpdateReportDto {
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  source: string;
+}
+
+export class ReportResponseDto {
+  id: string;
+  source: string;
+  amount: number;
+
+  @Exclude()
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+  type: ReportType;
+
+  @Expose({ name : "createdAt"})
+  transformedCreatedAt() {
+    return this.created_at;
+  }
+
+  constructor(partial: Partial<ReportResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
